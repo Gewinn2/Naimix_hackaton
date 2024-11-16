65b33c54dc35ec7f00933b1826097d5af6b5d24f
hnnsly <hynslei@ya.ru>
2024-11-16 17:55:50 +0300

    fixed log in updater

--- web/update.sh
+++ web/update.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-LOCKFILE="/tmp/auto_update.lock"
+LOCKFILE="/tmp/frontend_updater.lock"
 
 if [ -f "$LOCKFILE" ]; then
-  echo "Скрипт уже выполняется. Пропуск запуска."
+  echo "another instance is running!"
   exit 1
 fi
 
@@ -13,20 +13,18 @@ trap "rm -f $LOCKFILE; exit" INT TERM EXIT
 touch "$LOCKFILE"
 
 if [ ! -d ".git" ]; then
-  echo "Ошибка: Текущая директория не является Git-репозиторием."
+  echo "${pwd} is not a git-repository dir"
   rm -f "$LOCKFILE"
   exit 1
 fi
 
-echo "Проверка обновлений в репозитории..."
 git fetch origin
-
 
 LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse @{u})
 
 if [ "$LOCAL" != "$REMOTE" ]; then
-  echo "Найдены обновления. Обновление репозитория..."
+  echo "found update!"
 
   git stash
 
@@ -34,16 +32,13 @@ if [ "$LOCAL" != "$REMOTE" ]; then
 
 
   if [ -x "./deploy.sh" ]; then
-    echo "Запуск deploy.sh..."
+    echo "deploying..."
     ./deploy.sh
   else
-    echo "Скрипт deploy.sh не найден или не является исполняемым."
+    echo "deploy.sh not found/not executable"
   fi
 else
-  echo "Обновлений не найдено."
 fi
 
 rm -f "$LOCKFILE"
 trap - INT TERM EXIT
-
-echo "Скрипт завершён."
