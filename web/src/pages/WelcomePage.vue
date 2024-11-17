b61bf25f94725ba71d21d1f67e6fd413d2eda702
unwelcome <moyapohtaxd@mail.ru>
2024-11-17 04:51:28 +0300

    changed api

--- web/src/pages/WelcomePage.vue
+++ web/src/pages/WelcomePage.vue
@@ -1,5 +1,21 @@
 <template>
-  WelcomePage
+  <div class="flex flex-col flex-grow w-full items-center welcome-bg-image">
+    <div class="flex flex-row justify-between items-stretch w-10/12 h-full">
+      <div class="flex flex-col items-start">
+        <p class="">
+          Привет!
+          Ты - HR крупной компании и столкнулся с трудностями при отборе кандидатов? 
+          Или, может, ты только устраиваешься на работу и хочешь заглянуть в будущее?
+          Узнай, какие планы на ваше сотрудничество у судьбы!
+        </p>
+      </div>
+      <div>
+        <p>
+          Войди в свой аккаунт или зарегистрируй новый, если ты впервые на нашем сайте.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 export default{
